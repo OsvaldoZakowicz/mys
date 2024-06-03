622cbf73ce38bce740ab54c1ee2288444e01afe2
Osvaldo <osvaldozakowicz@gmail.com>
2024-06-03 00:19:49 -0300

    agrega funcion para recuperar todo el localstorage, mejora funciones de guardar item y obtener item, finaliza funcion guardar en index html de fibonacci

--- storage/localStorage.js
+++ storage/localStorage.js
@@ -1,4 +1,5 @@
 /**
+ * * guardar objeto en localstorage.
  * @param {string} clave string nombre del objeto a guardar
  * @param {object} objeto estructura de datos a guardar
  * @returns
@@ -14,26 +15,65 @@
  * }
  */
 function guardarEnLocalStorage(clave, objeto) {
+
     // local storage
     const lst = window.localStorage;
+
     // si ya existe un guardado con la misma clave
-    if (obtenerDeLocalStorage(clave) !== null) {
+    if (lst.getItem(clave) !== null) {
         return "ya existe una secuencia con ese nombre!";
     }
-    // guardado
-    lst.setItem(clave, JSON.stringify(objeto));
+
+    // guardar
+    try {
+        // puede generar error cuando se supera el espacio del local storage
+        // puede generar error cuando el usuario tiene deshabilitado el local storage
+        lst.setItem(clave, JSON.stringify(objeto));
+    } catch (error) {
+        return error.getMessage();
+    }
+
     return "secuencia guardada";
 }
 
 /**
- * 
+ * *obtener un objeto de localstorage
  * @param {string} clave string nombre del objeto a guardar
  * @returns {object} objeto guardado
  */
 function obtenerDeLocalStorage(clave) {
+
     // local storage
     const lst = window.localStorage;
-    // obtension
+    // obtener
     const objeto = lst.getItem(clave);
+
+    // validacion
+    if (objeto === null) {
+        return "la secuencia buscada no existe!";
+    }
+
     return objeto;
+}
+
+/**
+ * * obtiene todos los objetos del local storage
+ * @returns array de objetos
+ */
+function mostrarLocalStorage() {
+
+    // local storage
+    const lst = window.localStorage;
+    // array para secuencias obtenidas
+    const stored = [];
+
+    // recorrer por clave y obtener objetos
+    for (const clave in lst) {
+        if (Object.hasOwnProperty.call(lst, clave)) {
+            const element = lst[clave];
+            stored.push(element)
+        }
+    }
+
+    return stored;
 }
